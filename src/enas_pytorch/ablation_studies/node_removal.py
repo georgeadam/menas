c20f48c1b22e2983239d3cc74e8da032f3ec3263
alexadam <paddze21@gmail.com>
2018-11-06 15:05:46 -0500

    Fixed the node removal for the node ablation study

--- src/enas_pytorch/ablation_studies/node_removal.py
+++ src/enas_pytorch/ablation_studies/node_removal.py
@@ -20,18 +20,37 @@ from settings import ROOT_DIR
 logger = utils.get_logger()
 
 
-def remove_node(dag, remove_idx):
+def remove_node_hard(dag, remove_idx):
+    """
+    Given a DAG and an index to remove, this function creates a new DAG from scratch with the node having remove_idx
+    removed, as well as any nodes it feeds into, and any nodes that feed into it. This has a cascading effect for
+    the nodes that remove_idx feeds into, and the nodes after that, etc.
+
+    Args:
+        dag: A dictionary of Nodes.
+        remove_idx: The idx of the Node to be removed.
+
+    Returns:
+        A new DAG with the desired node, it's parents, child, grandchildren, etc. removed.
+    """
     # TODO (Alex): Fix to remove remaining nodes without parents
     new_dag = collections.defaultdict(list)
+    child_idxes = [remove_idx]
 
     for idx, nodes in dag.items():
         temp_nodes = []
 
-        if idx == remove_idx:
+        if idx in child_idxes:
+            child_idxes.remove(idx)
+
+            for node in nodes:
+                if node.name != 'avg' and node.name != 'h[t]':
+                    child_idxes.append(node.id)
+
             continue
 
         for node in nodes:
-            if node.id == remove_idx:
+            if node.id in child_idxes:
                 temp_node = None
             else:
                 temp_node = Node(node.id, node.name)
@@ -41,6 +60,49 @@ def remove_node(dag, remove_idx):
 
         if len(temp_nodes) > 0:
             new_dag[idx] = temp_nodes
+
+    return new_dag
+
+
+def remove_node_reconnect(dag, remove_idx):
+    """
+    Given a DAG and an index to remove, this function creates a new DAG from scratch with the node having remove_id
+    removed and its parents reconnected to its children when possible.
+
+    Args:
+        dag: A dictionary of Nodes.
+        remove_idx: The idx of the Node to be removed.
+
+    Returns:
+        A new DAG with the desired node removed, and its parents reconnected to its children.
+    """
+    new_dag = collections.defaultdict(list)
+    child_nodes = []
+    parent_nodes = []
+
+    for idx, nodes in dag.items():
+        temp_nodes = []
+
+        if idx == remove_idx:
+            for node in nodes:
+                child_nodes.append(Node(node.id, node.name))
+
+            continue
+
+        for node in nodes:
+            if node.id == remove_idx:
+                parent_nodes.append(Node(node.id, node.name))
+                temp_node = None
+            else:
+                temp_node = Node(node.id, node.name)
+
+            if temp_node is not None:
+                temp_nodes.append(temp_node)
+
+        if len(temp_nodes) > 0:
+            new_dag[idx] = temp_nodes
+
+    new_dag[parent_nodes[0].id] = child_nodes
 
     return new_dag
 
@@ -81,8 +143,10 @@ def main(args):  # pylint:disable=redefined-outer-name
     results = {}
 
     for idx in nodes:
-        temp_dag = remove_node(dag, idx)
+        temp_dag = remove_node_hard(dag, idx)
         ppl = trnr.get_perplexity_multibatch(trnr.eval_data, temp_dag, 1)
+
+        results[idx] = ppl
 
         print("Validation PPL when removed node {} is: {}".format(idx, ppl))
 
