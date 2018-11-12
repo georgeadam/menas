3ee693afd4c41578a134a6b45f281d8124d9843f
alexadam <paddze21@gmail.com>
2018-11-12 13:17:12 -0500

    Refactored ablation studies to use training configs
    -Things like dataset, model, num_blocks should have been coming from the training config that was generated
    when training ENAS, but instead I had it using the config for the ablation study itself which should only
    really be in charge of speciying the model path and gpu settings
    
    -Fixed the node removal study once and for all
    
    -Added tracking of test set performance (in addition to validation set performance) to the ablation studies
    
    -Added simple bash script to run ablation studies on slurm

--- src/enas_pytorch/configs/config_ablation.py
+++ src/enas_pytorch/configs/config_ablation.py
@@ -1,12 +1,16 @@
 from configs.config_orig import parser, data_arg, learn_arg, net_arg, misc_arg, logger
-from configs.config_orig import add_argument_group, str2bool, get_args_helper
+from configs.config_orig import add_argument_group, str2bool
+
+from dotenv import find_dotenv, load_dotenv
+from settings import ROOT_DIR
+
+import os
 
 # Network
 
 # Controller
 
 # Data
-data_arg.add_argument('--dataset', type=str, default='ptb')
 
 # Training / test parameters
 learn_arg.add_argument('--mode', type=str, default='derive',
@@ -24,9 +28,22 @@ misc_arg.add_argument('--num_gpu', type=int, default=1)
 misc_arg.add_argument('--num-workers', type=int, default=2)
 misc_arg.add_argument('--random_seed', type=int, default=12345)
 misc_arg.add_argument('--use_tensorboard', type=str2bool, default=True)
-misc_arg.add_argument("--train_type", type=str, default="enas",
-                      choices=['enas', 'random', 'hardcoded'])
 
 
 def get_args():
-    return get_args_helper(parser, logger)
+    args, unparsed = parser.parse_known_args()
+
+    d = vars(args)
+
+    load_dotenv(find_dotenv(), override=True)
+    d["data_dir"] = os.path.join(ROOT_DIR, args.data_dir)
+    d["log_dir"] = os.path.join(ROOT_DIR, args.log_dir)
+
+    if args.num_gpu > 0:
+        setattr(args, 'cuda', True)
+    else:
+        setattr(args, 'cuda', False)
+    if len(unparsed) > 1:
+        logger.info(f"Unparsed args: {unparsed}")
+
+    return args, unparsed
